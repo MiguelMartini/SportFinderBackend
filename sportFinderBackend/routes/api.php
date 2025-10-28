058ce9d54e045ebea220aab0a68a4d31781f4e05
MiguelMartini <miguelkaulingm21@gmail.com>
2025-10-28 15:22:07 -0300

    feat: Autenticacao usuarios

--- sportFinderBackend/routes/api.php
+++ sportFinderBackend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\UsersController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Middleware\ValidToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,5 +12,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('users', [UsersController::class, 'index']);
-Route::post('users', [UsersController::class, 'store']); 
-Route::patch('users/edit/{id}', [UsersController::class, 'update']); 
+// Route::post('users', [UsersController::class, 'store']); 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::delete('users/{id}', [UsersController::class, 'destroy']); 
+    Route::patch('users/edit/{id}', [UsersController::class, 'update']); 
+});
