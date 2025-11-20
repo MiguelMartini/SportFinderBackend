1d8e2a3bd050d40b1136ffee05665a759a4640f5
MiguelMartini <miguelkaulingm21@gmail.com>
2025-11-20 16:28:42 -0300

    Fix: Apenas respostas em JSON

--- sportFinderBackend/bootstrap/app.php
+++ sportFinderBackend/bootstrap/app.php
@@ -1,13 +1,17 @@
 <?php
 
+use App\Http\Middleware\ForceJsonResponse;
 use App\Http\Middleware\RoleMiddleware;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Exceptions\MissingAbilityException;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
@@ -16,8 +20,46 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             "role" => RoleMiddleware::class,
         ]);
-
+        $middleware->append(ForceJsonResponse::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, $request) {
+                return response()->json([
+                    'status' => 'Não autenticado',
+                    'message' => 'Token de autenticação inválido ou ausente.'
+                ], 401);
+        });
+
+        // Token Sanctum inválido ou sem permissão
+        $exceptions->render(function (MissingAbilityException $e, $request) {
+                return response()->json([
+                    'status' => 'Token inválido',
+                    'message' => 'O token fornecido é inválido ou não possui permissão.'
+                ], 401);
+        });
+
+        // Erros de validação
+        $exceptions->render(function (ValidationException $e, $request) {
+                return response()->json([
+                    'status' => 'Falha',
+                    'message' => $e->errors(),
+                ], 422);
+        });
+
+        // Model não encontrada (404)
+        $exceptions->render(function (ModelNotFoundException $e, $request) {
+                return response()->json([
+                    'status' => 'Não encontrado',
+                    'message' => 'O recurso solicitado não foi encontrado.'
+                ], 404);
+        });
+
+        // Erros inesperados (500)
+        $exceptions->render(function (Throwable $e, $request) {
+                return response()->json([
+                    'status' => 'Erro interno',
+                    'message' => 'Ocorreu um erro inesperado no servidor.',
+                    'error' => config('app.debug') ? $e->getMessage() : null,
+                ], 500);
+        });
     })->create();
