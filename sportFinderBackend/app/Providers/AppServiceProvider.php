d30f4d87e38851fb1239c1b28ab3789254ee2fd9
MiguelMartini <miguelkaulingm21@gmail.com>
2025-12-12 10:57:39 -0300

    Feat: Seguranca

--- sportFinderBackend/app/Providers/AppServiceProvider.php
+++ sportFinderBackend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('api-auth', function ($request) {
+            return Limit::perMinute(60)
+                ->by(optional($request->user())->id ?: $request->ip());
+        });
+
+        RateLimiter::for('api-public', function ($request) {
+            return Limit::perMinute(20)->by($request->ip());
+        });
     }
 }
